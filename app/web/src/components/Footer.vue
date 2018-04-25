bedf1bbf9bbfa9d61213278196494631de67b224
joshisumedha <33633023+joshisumedha@users.noreply.github.com>
2018-04-25 15:26:01 -0400

    CI CD

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure CI DI Sumedha
+        Final change CI CD Sumedha
       </div>
       <div class="col-lg-6">
       </div>
